4907173b10914b6c3a783793f858e1065a25db18
multiks2200 <multiks2200@gmail.com>
2017-02-25 08:40:02 +0000

    fixed a bug

--- logistic_regression/logistic_iteration.py
+++ logistic_regression/logistic_iteration.py
@@ -12,8 +12,8 @@ import seaborn as sns # More snazzy plotting library
 import itertools
 import warnings
 
-##test results 
-#  1.looks the absolute year number gives a good colleration for prediction. 
+##test results
+#  1.looks the absolute year number gives a good colleration for prediction.
 #  2. standarlistaion improves prediction by 3%
 #  3. removing aspect ratio improves prediction by 2%
 
@@ -27,7 +27,7 @@ for colname, colvalue in dta.iteritems():
     if type(colvalue[1]) == str:
         #if colname not in str_list:
             str_list.append(colname)
-            
+
 # Get to the numeric columns by inversion
 num_list = dta.columns.difference(str_list)
 #USe only the numeriv values
@@ -37,7 +37,7 @@ dta_clean = dta[num_list]
 dta_clean = dta_clean.dropna()
 dta_clean = dta_clean.reindex_axis(sorted(dta_clean.columns), axis=1)
 
-#clean up data from zero rows 
+#clean up data from zero rows
 for colname, colvalue in dta_clean.iteritems():
     if colname != 'facenumber_in_poster':
         dta_clean = dta_clean[dta_clean[colname] != 0]
@@ -56,62 +56,62 @@ dta_clean['gross_class'] = dta_clean.gross.apply (lambda gross: label_gross (gro
 def run_logistic_regression(yIn, xIn, solver, prct_split=0.3, scale=False, stadartlize=False, normalize=False, print_intr=False):
         #flatten the y vector
         yIn = np.ravel(yIn)
-        
+
         #do data tranformation
-        if scale: 
+        if scale:
             min_max_scaler = preprocessing.MinMaxScaler()
             for colname, colvalue in xIn.iteritems():
                 xIn[colname] = min_max_scaler.fit_transform(xIn[colname])
-        
-        if stadartlize:             
+
+        if stadartlize:
             for colname, colvalue in xIn.iteritems():
                 standard_scaler = preprocessing.StandardScaler().fit(xIn[colname])
                 xIn[colname] = standard_scaler.transform(xIn[colname])
 
-        if normalize: 
+        if normalize:
             for colname, colvalue in xIn.iteritems():
                 nomalizer_scaler = preprocessing.Normalizer().fit(xIn[colname])
-                xIn[colname] = nomalizer_scaler.transform(xIn[colname])[0] 
-                
+                xIn[colname] = nomalizer_scaler.transform(xIn[colname])[0]
+
         #train the model
         X_train, X_test, y_train, y_test = train_test_split(xIn, yIn, test_size=prct_split, random_state=0)
-        model = LogisticRegression()
+        model = LogisticRegression(solver=solver)
         model.fit(X_train, y_train)
-        
+
         # predict class labels for the test set
         predicted = model.predict(X_test)
-        
+
         # find the avarge score 10 iterations
         scores = cross_val_score(model, xIn, yIn, scoring='accuracy', cv=10)
-       
+
         #print findings
-        if print_intr: 
+        if print_intr:
             print "Running with scale: " + str(scale) + ", stadartlize: " + str(stadartlize) + ", normalize: " + str(normalize)
             print "Test data acuracy is: " + str(metrics.accuracy_score(y_test,predicted))
             print  "Avarage model score: " + str(scores.mean())
-        
+
         return metrics.accuracy_score(y_test,predicted), scores.mean()
 
 #define data trasormation itterator
 def run_logc_reg_with_data_trasn(yIn,xIn,prct_split, print_intr):
     prev_test_acc = 0
-    prev_avg_acc = 0 
+    prev_avg_acc = 0
     test_acc_str = "No Result"
     avg_ac_str = "No Result"
-    for solver in ['newton-cg', 'lbfgs', 'nliblinear', 'sag']:
+    for solver in ['newton-cg', 'lbfgs', 'liblinear', 'sag']:
         if print_intr:
             print "######################################################"
             print "Starting regressions sequance with solver: " + solver
-        for scale in [False, True]: 
+        for scale in [False, True]:
             for stadartlize in [False, True]:
                 for normalize in [False, True]:
                     test_acc, avg_acc = run_logistic_regression(yIn,xIn,solver, prct_split, scale, stadartlize, normalize, print_intr)
                     #save the highest accuracy result and result string
-                    if test_acc > prev_test_acc: 
+                    if test_acc > prev_test_acc:
                         prev_test_acc = test_acc
                         test_acc_str = "End highest test acc " + str(test_acc) + ", achieved with solver: " + solver + ", scale: " + str(scale) + ", stadartlize: " + str(stadartlize) + ", normalize: " + str(normalize)
                     if avg_acc  > prev_avg_acc :
-                        prev_avg_acc = avg_acc              
+                        prev_avg_acc = avg_acc
                         avg_ac_str = "End highest avg model acc " + str(avg_acc) + ", achieved with solver: " + solver + ", scale: " + str(scale) + ", stadartlize: " + str(stadartlize) + ", normalize: " + str(normalize)
         if print_intr: print "######################################################"
     print test_acc_str
@@ -121,6 +121,8 @@ def run_logc_reg_with_data_trasn(yIn,xIn,prct_split, print_intr):
 def run_diff_predictors():
     headers = list(dta_clean.columns.values)
     headers.remove('gross_class')
+    headers.remove('facenumber_in_poster')
+    headers.remove('aspect_ratio')
     headers.remove('gross')
     itter = 0
     prev_test_acc = 0
@@ -128,28 +130,28 @@ def run_diff_predictors():
     best_test_acc_str = "No results"
     best_avg_acc_str = "No results"
     iterator = 0
-    for L in range(0, len(headers)+1):
+    for L in range(4, len(headers)+1):
         for subset in itertools.combinations(headers, L):
             iterator = iterator + 1
     print "Running total " + str(iterator) + " itterators"
-    for L in range(0, len(headers)+1):
+    for L in range(4, len(headers)+1):
         for subset in itertools.combinations(headers, L):
-            string = "gross_class ~ " + " + ".join(str(x) for x in subset)            
+            string = "gross_class ~ " + " + ".join(str(x) for x in subset)
             if itter  > 0:
                 print "---------------------------------------------------------"
                 print str(itter) + " out of " + str(iterator) + " reg for the follow predictors:"
                 print(string)
                 y, X = dmatrices(string,dta_clean, return_type="dataframe")
-                test_acc, avg_acc = run_logc_reg_with_data_trasn(y,X, 0.3, print_intr=False)            
+                test_acc, avg_acc = run_logc_reg_with_data_trasn(y,X, 0.3, print_intr=False)
             #save best data
-                if test_acc > prev_test_acc: 
+                if test_acc > prev_test_acc:
                     prev_test_acc = test_acc
                     best_test_acc_str = "End highest test acc " + str(test_acc) + ", achieved with " + string
-                if avg_acc > prev_avg_acc: 
-                    prev_avg_acc = avg_acc              
+                if avg_acc > prev_avg_acc:
+                    prev_avg_acc = avg_acc
                     best_avg_acc_str = "End highest avg model acc " + str(avg_acc) + ", achieved with " + string
             itter = itter + 1
-            
+
     #dump out the results
     print "Ending the predictors"
     print best_test_acc_str
